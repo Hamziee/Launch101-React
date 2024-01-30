34b86bb2f2cfc91a1b74a3b4327940d02b638766
Hamza <hamza@heo-systems.com>
2024-01-30 22:54:42 +0100

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ import {
 } from 'react-router-dom'
 
 import './style.css'
+import LatestVideo from './views/latest-video'
 import Download from './views/download'
 import Home from './views/home'
-import LatestVideo from './views/latest-video'
 import NotFound from './views/not-found'
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route component={LatestVideo} exact path="/latest-video" />
         <Route component={Download} exact path="/download" />
         <Route component={Home} exact path="/" />
-        <Route component={LatestVideo} exact path="/latest-video" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
